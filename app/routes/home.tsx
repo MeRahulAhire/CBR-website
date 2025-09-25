0c3fee7eb398ddc6f798c567ac4ef9d258119049
Rahul Ahire <info@rahulahire>
2025-09-25 23:23:14 +0530

    section 1 of landing page is finished

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,27 @@
+import "../../style/home.css"
+
 export default function home() {
-  return <div>home</div>;
+  return <>
+  
+  <div className="lp-s1">
+    <div className="s1-subsec-1">
+      <div className="subsec-1-container">
+        <div className="subsec-1-head">Cloud Blender Render</div>
+        <div className="subsec-1-head2">Smarter, affordable and open-source rendering</div>
+        <div className="subsec-1-p">
+          <p>RTX 5090 from $0.69/hour</p>
+          <p>RTX 4090 from $0.34/hour</p>
+        </div>
+        <div className="subsec-1-btn-deploy">
+          Deploy Now
+        </div>
+      <div className="subsec-1-free-credit">
+        Signup with our link and get additional $5-500 for free
+      </div>
+      </div>
+    </div>
+    <div className="s1-subsec-2"></div>
+  </div>
+  
+  </>;
 }
